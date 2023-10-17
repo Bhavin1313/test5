3b0d4385ef93bb8e007707c84fd48597d73d0774
nidhijoshi14 <93935753+nidhijoshi14@users.noreply.github.com>
2023-10-17 10:16:34 +0530

    Api

---
+++ lib/providers/api.dart
@@ -0,0 +1,8 @@
+//NewsApi
+//https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4324c877736c4c0e8c1f34cc72776420
+
+//Quotes
+//https://type.fit/api/quotes
+
+//Country
+//https://restcountries.com/v3.1/all
